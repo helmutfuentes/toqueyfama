86178de5eb0f43b636fa2d5f945d1ddf11c30359
HenuXMail <HenuXMail@fbb96c9a-e064-11de-b950-c1e2c078a2c5>
2009-12-05 00:52:36 +0000

    agregando algunas cosillas :)

---
+++ ToqueYFama3/src/Ventana.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrador
+ */
+public class Ventana {
+
+    boolean mostrandoVentana;
+
+    public boolean mostrar(boolean b){
+    mostrandoVentana=b;
+    return mostrandoVentana;
+    }
+}
